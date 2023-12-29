c79e50dd7d00d05754137a3c696bba0b56a98c7e
Björn Frischknecht <bjoern@frischknecht-online.ch>
2023-12-29 20:15:18 +0100

    korrektur

--- contact2wildix.py
+++ contact2wildix.py
@@ -1,7 +1,5 @@
 #CSV import in Wildix PBX Telefonbücher - die Konfig muss im config.py angepasst werden
-de_DE       -->  # -*- coding: iso-8859-1 -*-
-de_DE@euro  -->  # -*- coding: iso-8859-15 -*-
-de_DE.utf8  -->  # -*- coding: utf-8 -*-
+# -*- coding: iso-8859-1 -*-
 
 import csv
 import requests
